f3da3d464d11ba4b539a9a39b944530f9e884c90
ItosEO <m2265514616@163.com>
2026-04-15 22:15:51 +0800

    [prerelease] v1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ if (ksFile.canRead()) {
 }
 
 val gitCommitCountInt = rootProject.extra["gitCommitCountInt"] as Int
-val baseVersionName = "1.5.1"
+val baseVersionName = "1.5.2"
 val versionNameSuffixProvider = providers.gradleProperty("versionNameSuffix")
     .orElse("-alpha$gitCommitCountInt")
 val finalVersionNameProvider = versionNameSuffixProvider.map { suffix ->
